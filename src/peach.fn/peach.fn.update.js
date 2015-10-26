ce48b6d7f4a54b17f8a9a1c9940f2bbc81e44e0f
Sean MacIsaac <seanjmacisaac@gmail.com>
2015-10-26 16:14:45 -0400

    Fixed bug in `peach.fn.update.js'

--- src/peach.fn/peach.fn.update.js
+++ src/peach.fn/peach.fn.update.js
@@ -13,7 +13,7 @@ Peach.fn.update = function (name, object) {
   }
   dataBinding[name] = newList;
   _.forEach(dataBinding[name], function (oldNode, i) {
-    var newNode = peach.node(name, object);
+    var newNode = Peach.fn.node.call(self, name, object);
     oldNode.parentNode.replaceChild(newNode, oldNode);
     self.bind(name, newNode);
   });
